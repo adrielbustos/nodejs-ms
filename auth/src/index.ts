afd8b6b71aa49c5731d1135fc814d153094adbac
Adriel Bustos <adriel.bustos01@gmail.com>
2022-04-29 00:58:32 -0300

    kubernete config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,9 +2,14 @@ import express from "express";
 import { json } from "body-parser";
 
 // npm install typescript ts-node-dev
+// tsc --init
 
 const app = express();
 app.use(json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi!");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
