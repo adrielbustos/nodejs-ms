66c621cdbacfb6c20e62a075b21ccb37229ef69d
Adriel Bustos <adriel.bustos01@gmail.com>
2022-05-12 21:59:32 -0300

    error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,28 @@
 import express from "express";
 import { json } from "body-parser";
 
-// npm install typescript ts-node-dev
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+// npm install typescript ts-node-dev express
 // tsc --init
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi!");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use("*", () => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
