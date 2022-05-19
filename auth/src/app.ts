fd78d5b732b09600e4be9d6245ebaf502592efa3
Adriel Bustos <adriel.bustos01@gmail.com>
2022-05-19 20:30:34 -0300

    create a common package with a public npm

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -9,8 +9,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler, NotFoundError } from "@abgdev/common";
 // npm install typescript ts-node-dev express
 // tsc --init
 
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@abgdev/common";
 
 const router = express.Router();
 
